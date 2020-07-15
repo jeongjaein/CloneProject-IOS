6a0dcf3c4557b6dd182eb7b1589a5d2c95fe2eaa
jeongjaein <wodls96@gmail.com>
2020-07-15 19:53:39 +0900

    Create CloneProject - 1 : Instagram

---
+++ CloneProject - 1 : Instagram/RootPageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RootPageViewController.swift
+//  CloneProject - 1 : Instagram
+//
+//  Created by 정재인 on 2020/07/15.
+//  Copyright © 2020 정재인. All rights reserved.
+//
+
+import UIKit
+
+class RootPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
